0d693c728117528f84146173df7c9b5a2c3a5a73
Praveen Gunasekaran <Praveenraj49@gmail.com>
2018-08-09 00:27:30 -0700

    Fixed Compilation

--- CarND-Semantic-Segmentation/main.py
+++ CarND-Semantic-Segmentation/main.py
@@ -7,16 +7,6 @@ from distutils.version import LooseVersion
 import project_tests as tests
 
 
-# Check TensorFlow Version
-assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
-print('TensorFlow Version: {}'.format(tf.__version__))
-
-# Check for a GPU
-if not tf.test.gpu_device_name():
-    warnings.warn('No GPU found. Please use a GPU to train your neural network.')
-else:
-    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
-
 
 def load_vgg(sess, vgg_path):
     """
@@ -45,7 +35,7 @@ def load_vgg(sess, vgg_path):
 
     
     return image_input, keep_prob, layer3, layer4, layer7
-tests.test_load_vgg(load_vgg, tf)
+
 
 
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
@@ -58,31 +48,36 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    conv_1x1_7 = tf.layers.conv2d(vgg_layer7_out , num_classes , 1, padding = 'same',
-                                kernel_regularizer = tf.contrib.layers.l2_regularizer(1e-3))
+    init = tf.random_normal_initializer(stddev=0.01)
 
-    output_7 =  tf.layers.conv2d_transpose(conv_1x1_7 , num_classes , 4, 2 , padding = 'same',
-                                         kernel_regularizer = tf.contrib.layers.l2_regularizer(1e-3))
+    conv_1x1_7 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same',
+                               kernel_initializer=init, kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     conv_1x1_4 = tf.layers.conv2d(vgg_layer4_out , num_classes , 1, padding = 'same',
-                                kernel_regularizer = tf.contrib.layers.l2_regularizer(1e-3))
+                                kernel_initializer=init,kernel_regularizer = tf.contrib.layers.l2_regularizer(1e-3))
 
-    input_4 = tf.add(output_7, conv_1x1_4)
-
-    output_4 = tf.layers.conv2d_transpose(input_4 , num_classes, 4, 2 , padding = 'same',
-                                        kernel_regularizer = tf.contrib.layers.l2_regularizer(1e-3))
-
-    conv_1x1_3  = tf.layers.conv2d(vgg_layer3_out , num_classes ,1, padding = 'same',
-                                kernel_regularizer = tf.contrib.layers.l2_regularizer(1e-3))
-
-    input_3 = tf.add(output_4 , conv_1x1_3)
-
-    output = tf.layers.conv2d_transpose(input_3 , num_classes , 16 , 8, padding = 'same',
-                                        kernel_regularizer = tf.contrib.layers.l2_regularizer(1e-3))
+    conv_1x1_3 = tf.layers.conv2d(vgg_layer3_out , num_classes, 1, padding='same',
+                                kernel_initializer=init, kernel_regularizer = tf.contrib.layers.l2_regularizer(1e-3))
 
 
-    return output
-tests.test_layers(layers)
+    deconv_1 =  tf.layers.conv2d_transpose(conv_1x1_7, num_classes, 4, 2 , padding='same',
+                                         kernel_initializer=init, kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+
+    skip_1 = tf.add(deconv_1, conv_1x1_4)
+
+    deconv_2  = tf.layers.conv2d_transpose(skip_1 , num_classes, 4, 2 , padding='same',
+                                kernel_initializer=init, kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+
+    skip_2 = tf.add(deconv_2, conv_1x1_3)
+
+    deconv_3 = tf.layers.conv2d_transpose(skip_2 , num_classes, 16, 8, padding='same',
+                                kernel_initializer=init, kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+
+    return deconv_3
+
 
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
@@ -97,11 +92,11 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     # TODO: Implement function
     logits  = tf.reshape(nn_last_layer ,(-1, num_classes))
     correct_label = tf.reshape(correct_label,(-1, num_classes))
-    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits= logits,labels = correct_label))
-    optimizer  = tf.train.AdamOptimizer(learning_rate = learning_rate)
-    train_op = optimizer.minimize(cross_entropy_loss);
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_op = optimizer.minimize(cross_entropy_loss)
     return logits, train_op, cross_entropy_loss
-tests.test_optimize(optimize)
+
 
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
@@ -127,13 +122,15 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
 
     for epoch in range(epochs):
         print("EPOCH {} .....".format(epoch+1))
-        for image ,label in get_batches_fn(batch_size):
+        for image,label in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss],
-                               feed_dict={input_image: image, correct_label: label, keep_prob: 0.5,
-                                           learning_rate: 0.0009})
+                               feed_dict={input_image: image,
+                                          correct_label: label,
+                                          keep_prob: 0.5,
+                                          learning_rate: 1e-4})
             print("Loss: = {:.3f}".format(loss))
 
-tests.test_train_nn(train_nn)
+
 
 
 def run():
@@ -141,7 +138,7 @@ def run():
     image_shape = (160, 576)
     data_dir = './data'
     runs_dir = './runs'
-    tests.test_for_kitti_dataset(data_dir)
+
 
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
@@ -161,21 +158,22 @@ def run():
 
         # TODO: Build NN using load_vgg, layers, and optimize function
 
-        epochs  = 50
-        batch_size = 5
+        epochs = 10
+        batch_size = 1
+        learning_rate = tf.constant(1e-4)
 
-        correct_label = tf.placeholder(tf.int32 , [None, None, None , num_classes] , name = 'correct_label')
-        learning_rate = tf.placeholder(tf.float32 , name = 'learning_rate')
+        correct_label = tf.placeholder(tf.float32, [None, image_shape[0], image_shape[1], num_classes])
+        #learning_rate = tf.placeholder(tf.float32, name='learning_rate')
 
-        input_image , keep_prob , vgg_layer3, vgg_layer4, vgg_layer_7 = load_vgg(sess,vgg_path)
+        input_image, keep_prob, vgg_layer3, vgg_layer4, vgg_layer_7 = load_vgg(sess,vgg_path)
 
-        nn_last_layer = layers(vgg_layer3, vgg_layer4 , vgg_layer_7 , num_classes)
+        nn_last_layer = layers(vgg_layer3, vgg_layer4, vgg_layer_7, num_classes)
 
-        logits , train_op , cross_entropy_loss = optimize(nn_last_layer,correct_label,learning_rate,num_classes)
+        logits, train_op, cross_entropy_loss = optimize(nn_last_layer,correct_label,learning_rate,num_classes)
 
         # TODO: Train NN using the train_nn function
 
-        train_nn(sess,epochs,batch_size, get_batches_fn,train_nn ,cross_entropy_loss,input_image,correct_label,
+        train_nn(sess,epochs,batch_size, get_batches_fn,train_op ,cross_entropy_loss,input_image,correct_label,
                  keep_prob,learning_rate)
 
         # TODO: Save inference data using helper.save_inference_samples
@@ -185,4 +183,25 @@ def run():
 
 
 if __name__ == '__main__':
+
+    # Check TensorFlow Version
+    assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
+    print('TensorFlow Version: {}'.format(tf.__version__))
+
+    # Check for a GPU
+    if not tf.test.gpu_device_name():
+        warnings.warn('No GPU found. Please use a GPU to train your neural network.')
+    else:
+        print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+
+
+    print("# Running Tests")
+    data_dir = './data'
+    tests.test_for_kitti_dataset(data_dir)
+    tests.test_load_vgg(load_vgg, tf)
+    tests.test_layers(layers)
+    tests.test_optimize(optimize)
+    tests.test_train_nn(train_nn)
+
+    print("Model Run Started")
     run()
